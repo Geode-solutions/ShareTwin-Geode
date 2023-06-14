658c8ba8fa10faea9b38f8fcf084dcc1e624cb38
JulienChampagnol <julien.champagnol@geode-solutions.com>
2023-06-14 11:13:50 +0200

    Add crs/is_model/is_3D in geode_objects.py

--- geode_objects.py
+++ geode_objects.py
@@ -12,6 +12,12 @@ def objects_list():
             'load': og.load_brep,
             'save': og.save_brep,
             'builder': og.BRepBuilder,
+            'crs': {
+                'assign': og_gs.assign_brep_geographic_coordinate_system_info,
+                'convert': og_gs.convert_brep_coordinate_reference_system
+            },
+            'is_model': True,
+            'is_3D': True,
             'is_viewable': True,
             'save_viewable': g_v.save_viewable_brep
         },
@@ -21,6 +27,12 @@ def objects_list():
             'load': og_gs.load_cross_section,
             'save': og_gs.save_cross_section,
             'builder': og_gs.CrossSectionBuilder,
+            'crs': {
+                'assign': og_gs.assign_section_geographic_coordinate_system_info,
+                'convert': og_gs.convert_section_coordinate_reference_system
+            },
+            'is_model': True,
+            'is_3D': False,
             'is_viewable': True,
             'save_viewable': g_v.save_viewable_cross_section
         },
@@ -30,6 +42,12 @@ def objects_list():
             'load': og.load_edged_curve2D,
             'save': og.save_edged_curve2D,
             'builder': og.EdgedCurveBuilder2D.create,
+            'crs': {
+                'assign': og_gs.assign_edged_curve_geographic_coordinate_system_info2D,
+                'convert': og_gs.convert_edged_curve_coordinate_reference_system2D
+            },
+            'is_model': False,
+            'is_3D': False,
             'is_viewable': True,
             'save_viewable': g_v.save_viewable_edged_curve2D
         },
@@ -39,6 +57,12 @@ def objects_list():
             'load': og.load_edged_curve3D, 
             'save': og.save_edged_curve3D,
             'builder': og.EdgedCurveBuilder3D.create,
+            'crs': {
+                'assign': og_gs.assign_edged_curve_geographic_coordinate_system_info3D,
+                'convert': og_gs.convert_edged_curve_coordinate_reference_system3D
+            },
+            'is_model': False,
+            'is_3D': True,
             'is_viewable': True,
             'save_viewable': g_v.save_viewable_edged_curve3D
         },
@@ -47,7 +71,9 @@ def objects_list():
             'output': [ og.GraphOutputFactory ], 
             'load': og.load_graph, 
             'save': og.save_graph, 
-            'builder': og.GraphBuilder.create, 
+            'builder': og.GraphBuilder.create,
+            'is_model': False,
+            'is_3D': False,
             'is_viewable': True,
             'save_viewable': ''
         },
@@ -57,6 +83,12 @@ def objects_list():
             'load': og.load_hybrid_solid3D, 
             'save': og.save_hybrid_solid3D, 
             'builder': og.HybridSolidBuilder3D.create, 
+            'crs': {
+                'assign': og_gs.assign_solid_mesh_geographic_coordinate_system_info3D,
+                'convert': og_gs.convert_solid_mesh_coordinate_reference_system3D
+            },
+            'is_model': False,
+            'is_3D': True,
             'is_viewable': True,
             'save_viewable': g_v.save_viewable_hybrid_solid3D
         },
@@ -66,6 +98,12 @@ def objects_list():
             'load': og.load_point_set2D, 
             'save': og.save_point_set2D, 
             'builder': og.PointSetBuilder2D.create,
+            'crs': {
+                'assign': og_gs.assign_point_set_geographic_coordinate_system_info2D,
+                'convert': og_gs.convert_point_set_coordinate_reference_system2D
+            },
+            'is_model': False,
+            'is_3D': False,
             'is_viewable': True,
             'save_viewable': g_v.save_viewable_point_set2D
         },
@@ -75,6 +113,12 @@ def objects_list():
             'load': og.load_point_set3D, 
             'save': og.save_point_set3D, 
             'builder': og.PointSetBuilder3D.create,
+            'crs': {
+                'assign': og_gs.assign_point_set_geographic_coordinate_system_info3D,
+                'convert': og_gs.convert_point_set_coordinate_reference_system3D
+            },
+            'is_model': False,
+            'is_3D': True,
             'is_viewable': True,
             'save_viewable': g_v.save_viewable_point_set3D
         },
@@ -84,6 +128,12 @@ def objects_list():
             'load': og.load_polygonal_surface2D, 
             'save': og.save_polygonal_surface2D, 
             'builder': og.PolygonalSurfaceBuilder2D.create,
+             'crs': {
+                'assign': og_gs.assign_surface_mesh_geographic_coordinate_system_info2D,
+                'convert': og_gs.convert_surface_mesh_coordinate_reference_system2D
+            },
+            'is_model': False,
+            'is_3D': False,
             'is_viewable': True,
             'save_viewable': g_v.save_viewable_polygonal_surface2D
         },
@@ -93,6 +143,12 @@ def objects_list():
             'load': og.load_polygonal_surface3D, 
             'save': og.save_polygonal_surface3D, 
             'builder': og.PolygonalSurfaceBuilder3D.create,
+            'crs': {
+                'assign': og_gs.assign_surface_mesh_geographic_coordinate_system_info3D,
+                'convert': og_gs.convert_surface_mesh_coordinate_reference_system3D
+            },
+            'is_model': False,
+            'is_3D': True,
             'is_viewable': True,
             'save_viewable': g_v.save_viewable_polygonal_surface3D
         },
@@ -102,6 +158,12 @@ def objects_list():
             'load': og.load_polyhedral_solid3D, 
             'save': og.save_polyhedral_solid3D, 
             'builder': og.PolyhedralSolidBuilder3D.create, 
+            'crs': {
+                'assign': og_gs.assign_solid_mesh_geographic_coordinate_system_info3D,
+                'convert': og_gs.convert_solid_mesh_coordinate_reference_system3D
+            },
+            'is_model': False,
+            'is_3D': True,
             'is_viewable': True,
             'save_viewable': g_v.save_viewable_polyhedral_solid3D
         },
@@ -110,6 +172,8 @@ def objects_list():
             'output': [ og.RasterImageOutputFactory2D ], 
             'load': og.load_raster_image2D, 
             'save': og.save_raster_image2D,
+            'is_model': False,
+            'is_3D': False,
             'is_viewable': False,
             'save_viewable': g_v.save_viewable_raster_image2D
         },
@@ -118,6 +182,8 @@ def objects_list():
             'output': [ og.RasterImageOutputFactory3D ], 
             'load': og.load_raster_image3D, 
             'save': og.save_raster_image3D,
+            'is_model': False,
+            'is_3D': False,
             'is_viewable': False,
             'save_viewable': g_v.save_viewable_raster_image3D
         },
@@ -127,6 +193,12 @@ def objects_list():
             'load': og.load_regular_grid2D, 
             'save': og.save_regular_grid2D, 
             'builder': og.RegularGridBuilder2D.create,
+            'crs': {
+                'assign': og_gs.assign_surface_mesh_geographic_coordinate_system_info2D,
+                'convert': og_gs.convert_surface_mesh_coordinate_reference_system2D
+            },
+            'is_model': False,
+            'is_3D': False,
             'is_viewable': True,
             'save_viewable': g_v.save_viewable_regular_grid2D
         },
@@ -136,6 +208,12 @@ def objects_list():
             'load': og.load_regular_grid3D, 
             'save': og.save_regular_grid3D, 
             'builder': og.RegularGridBuilder3D.create,
+            'crs': {
+                'assign': og_gs.assign_solid_mesh_geographic_coordinate_system_info3D,
+                'convert': og_gs.convert_solid_mesh_coordinate_reference_system3D
+            },
+            'is_model': False,
+            'is_3D': True,
             'is_viewable': True,
             'save_viewable': g_v.save_viewable_regular_grid3D
         },
@@ -145,6 +223,12 @@ def objects_list():
             'load': og.load_section, 
             'save': og.save_section, 
             'builder': og.SectionBuilder,
+            'crs': {
+                'assign': og_gs.assign_section_geographic_coordinate_system_info,
+                'convert': og_gs.convert_section_coordinate_reference_system
+            },
+            'is_model': True,
+            'is_3D': False,
             'is_viewable': True,
             'save_viewable': g_v.save_viewable_section 
         },
@@ -153,7 +237,13 @@ def objects_list():
             'output': [ og.BRepOutputFactory, og_gs.StructuralModelOutputFactory ], 
             'load': og_gs.load_structural_model, 
             'save': og_gs.save_structural_model, 
-            'builder': og_gs.StructuralModelBuilder, 
+            'builder': og_gs.StructuralModelBuilder,
+            'crs': {
+                'assign': og_gs.assign_brep_geographic_coordinate_system_info,
+                'convert': og_gs.convert_brep_coordinate_reference_system
+            },
+            'is_model': True,
+            'is_3D': True,
             'is_viewable': True,
             'save_viewable': g_v.save_viewable_structural_model
         },
@@ -162,7 +252,13 @@ def objects_list():
             'output': [ og.TetrahedralSolidOutputFactory3D ], 
             'load': og.load_tetrahedral_solid3D,
             'save': og.save_tetrahedral_solid3D, 
-            'builder': og.TetrahedralSolidBuilder3D.create, 
+            'builder': og.TetrahedralSolidBuilder3D.create,
+            'crs': {
+                'assign': og_gs.assign_solid_mesh_geographic_coordinate_system_info3D,
+                'convert': og_gs.convert_solid_mesh_coordinate_reference_system3D
+            },
+            'is_model': False,
+            'is_3D': True,
             'is_viewable': True,
             'save_viewable': g_v.save_viewable_tetrahedral_solid3D
         },
@@ -172,6 +268,12 @@ def objects_list():
             'load': og.load_triangulated_surface2D, 
             'save': og.save_triangulated_surface2D, 
             'builder': og.TriangulatedSurfaceBuilder2D.create, 
+            'crs': {
+                'assign': og_gs.assign_surface_mesh_geographic_coordinate_system_info2D,
+                'convert': og_gs.convert_surface_mesh_coordinate_reference_system2D
+            },
+            'is_model': False,
+            'is_3D': False,
             'is_viewable': True,
             'save_viewable': g_v.save_viewable_triangulated_surface2D
         },
@@ -181,6 +283,12 @@ def objects_list():
             'load': og.load_triangulated_surface3D, 
             'save': og.save_triangulated_surface3D, 
             'builder': og.TriangulatedSurfaceBuilder3D.create, 
+            'crs': {
+                'assign': og_gs.assign_surface_mesh_geographic_coordinate_system_info3D,
+                'convert': og_gs.convert_surface_mesh_coordinate_reference_system3D
+            },
+            'is_model': False,
+            'is_3D': True,
             'is_viewable': True,
             'save_viewable': g_v.save_viewable_triangulated_surface3D
         },
@@ -189,7 +297,9 @@ def objects_list():
             'output': [ og.VertexSetOutputFactory ], 
             'load': og.load_vertex_set, 
             'save': og.save_vertex_set, 
-            'builder': og.VertexSetBuilder.create, 
+            'builder': og.VertexSetBuilder.create,
+            'is_model': True,
+            'is_3D': False,
             'is_viewable': False,
             'save_viewable': ''
         }
